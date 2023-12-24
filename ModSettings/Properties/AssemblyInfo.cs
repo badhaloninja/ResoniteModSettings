d157cec699db8d58523b88813f7687f34acf5ad1
badhaloninja <badhaloninja@badhalo.ninja>
2023-12-23 21:53:54 -0600

    Re-add assemblyinfo.cs for missing metadata

---
+++ ModSettings/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("Resonite mod that adds a dash screen to manage mods")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("CE8BB933-8E61-419C-A334-FA3CFC5E4E72")]
+
+// The rest of the ususal metadata is handled by the csproj files
