c8d9155036f2ec0acb4c6a13389354a669b5d654
art0007i <art0007i@gmail.com>
2024-10-06 09:29:31 +0200

    working custom ui draft

--- ModSettings/FoundMod.cs
+++ ModSettings/FoundMod.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 
 using HarmonyLib;
 using ResoniteModLoader;
+using FrooxEngine.UIX;
 
 namespace ModSettings
 {
     internal record FoundMod()
     {
+        private static Harmony harmony = new ("ninja.badhalo.ModSettingsCustomUi");
+
         public FoundMod(ResoniteModBase owner) : this()
         {
             Owner = owner;
@@ -20,10 +23,30 @@ namespace ModSettings
             // Prepopulate all of the keys with null field info, for manually defined keys
             Config.ConfigurationItemDefinitions.Do(k => ConfigKeyFields[k] = null);
 
+            var modType = Owner.GetType();
+
             // Go over the fields to store the config field info
-            var fields = AccessTools.GetDeclaredFields(Owner.GetType());
+            var fields = AccessTools.GetDeclaredFields(modType);
             // Only get the config key fields and store the fields with their keys
             fields.Where(IsConfigurationKeyField).Do(StoreFieldWithKey);
+
+            // Inject Custom Ui Stuff
+            foreach (var apiMethod in ModSettings.ModSettingsScreen.PublicApiMethods)
+            {
+                var method = AccessTools.DeclaredMethod(modType, apiMethod.Key);
+                if (method == null) continue;
+                
+                // The public api methods has 5 methods, 4 are implemented in this mod, and one is just the entry point
+                if (apiMethod.Value.Item1 == null)
+                {
+                    BuildCustomUI = (ui) => method.Invoke(owner, [ui]);
+                }
+                else
+                {
+                    // According to harmony docs __args has a small performance overhead, so unrolling this loop and removing that might be useful
+                    harmony.Patch(method, new HarmonyMethod(apiMethod.Value.Item2));
+                }
+            }
         }
 
         public ResoniteModBase Owner { get; private set; }
@@ -31,6 +54,7 @@ namespace ModSettings
 
         public Dictionary<ModConfigurationKey, FieldInfo> ConfigKeyFields { get; private set; } = [];
 
+        public Action<UIBuilder>? BuildCustomUI;
 
 
         private static bool IsConfigurationKeyField(FieldInfo field) => Attribute.GetCustomAttribute(field, typeof(AutoRegisterConfigKeyAttribute)) != null;
