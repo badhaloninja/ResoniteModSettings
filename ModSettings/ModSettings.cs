3ab85cef9cbc245b78984558dce6b857fbd13585
badhaloninja <badhaloninja@badhalo.ninja>
2025-04-03 22:13:43 -0500

    v2.2.2 Rebuild for 2025.4.3.1346 closes #9

--- ModSettings/ModSettings.cs
+++ ModSettings/ModSettings.cs
@@ -15,7 +15,7 @@ namespace ModSettings
     {
         public override string Name => "ResoniteModSettings";
         public override string Author => "badhaloninja";
-        public override string Version => "2.2.1";
+        public override string Version => "2.2.2";
         public override string Link => "https://github.com/badhaloninja/ResoniteModSettings";
 
         [AutoRegisterConfigKey]
